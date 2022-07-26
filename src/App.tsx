f3cc72fbaac7f240e2b826e8a671581421d9c916
Toye Oloko <toyeoloko@Toyes-MacBook-Pro.local>
2022-07-26 11:24:23 -0400

    toye's hack

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect,useState } from "react";
 import { useSelector } from "react-redux";
 
 import "./App.scss";
 
+import GenderCharacter from "./Components/GenderCharacter";
 import ReactiveParagraph from "./Components/ReactiveParagraph";
 import ReactiveGraph from "./Components/ReactiveGraph";
 import RawData from "./Components/RawData";
@@ -20,8 +21,17 @@ import { RootStoreI } from "./Store";
 function App() {
   const { sourced } = useSelector((store: RootStoreI) => store.dataReducer);
   const scrollDestination = useRef<null | HTMLDivElement>(null);
+  const [data, setData] = useState({})
 
   useEffect(() => {
+   // fetch("http://127.0.0.1:5000/result").then(
+     // res => res.json()
+    //).then(
+     // data => {
+      //  setData(data)
+      //  console.log(data)
+     // }
+   // )
     if (sourced) {
       scrollDestination.current?.scrollIntoView({
         behavior: "smooth",
@@ -37,7 +47,8 @@ function App() {
           <ReportSectionLabel text="Filter" />
           <Filter></Filter>
           <ReportDivider />
-
+          <GenderCharacter/>
+          <ReportDivider/>
           <ReportSectionLabel text="Story" />
           <ReactiveParagraph></ReactiveParagraph>
           <ReportDivider />
