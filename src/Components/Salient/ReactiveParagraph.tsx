0d469043ba225771788ad56989facec4b72b4d36
Moshi <li.moshi95@gmail.com>
2022-07-31 18:33:27 -0400

    Added Salient Evnt functionalities

--- src/Components/Salient/ReactiveParagraph.tsx
+++ src/Components/Salient/ReactiveParagraph.tsx
@@ -1,44 +1,100 @@
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
-
-import { RootStoreI, useAppDispatch } from "../../Store";
+import { EventI, EventSalientInfoI } from "../../Slices/DataSlice";
+import { RootStoreI } from "../../Store";
 
 import "./ReactiveParagraph.scss";
 
-const ReactiveParagraph = () => {
-  const { paragraph, textOccurrenceMap } = useSelector(
-    (store: RootStoreI) => store.dataReducer
+const generateContent = (
+  eventListInput: EventI[],
+  eventSalientInfo: EventSalientInfoI[],
+  paragraph: string,
+  gender: "male" | "female" | "mix"
+) => {
+  let eventList = JSON.parse(JSON.stringify(eventListInput)) as EventI[];
+  let result: any = {};
+  const salientInfoMap: any = {};
+  eventSalientInfo.forEach(
+    (info) =>
+      (salientInfoMap[info.sentenceId] = { [info.eventId]: info.temporalRank })
   );
 
-  const content = [];
-  let leftBound = 0;
-  let rightBound = 0;
-  while (rightBound < paragraph.length) {
-    if (
-      textOccurrenceMap[rightBound] &&
-      textOccurrenceMap[rightBound].type === "event"
-    ) {
-      content.push(
-        <React.Fragment key={leftBound}>
-          {paragraph.substring(leftBound, rightBound)}
+  eventList
+    .filter((event) => {
+      return (
+        salientInfoMap[event.sentenceId] &&
+        salientInfoMap[event.sentenceId][event.eventId]
+      );
+    })
+    .forEach((event) => {
+      const key = `${event.verbStartByteText}:${event.verbEndByteText}`;
+      if (!result[key]) {
+        result[key] = event.gender;
+      } else if (result[key] !== event.gender) {
+        result[key] = "mix";
+      }
+    });
+  result = Object.keys(result)
+    .map((key) => {
+      return [parseInt(key.split(":")[0]), key.split(":")[1], result[key]];
+    })
+    .filter((item) => {
+      return item[2] === gender;
+    });
+  result.sort((a: any, b: any) => {
+    return a[0] - b[0];
+  });
+
+  let index = 0;
+  result = result
+    .map((item: any) => {
+      const [startIndex, endIndex, gender] = item;
+      const text = (
+        <React.Fragment>
+          {paragraph.substring(index, startIndex)}
         </React.Fragment>
       );
-      leftBound = rightBound;
-      rightBound = textOccurrenceMap[rightBound].textEndIndex;
-      content.push(
-        <span style={{ backgroundColor: "red" }} key={leftBound}>
-          {paragraph.substring(leftBound, rightBound)}
+      const span = (
+        <span
+          style={{
+            backgroundColor:
+              gender === "male"
+                ? "blue"
+                : gender === "female"
+                ? "red"
+                : "yellow",
+          }}
+        >
+          {paragraph.substring(startIndex, endIndex)}
         </span>
       );
-      leftBound = rightBound;
-    } else {
-      rightBound++;
-    }
-  }
+      index = endIndex;
+      return [text, span];
+    })
+    .flat();
+  result.push(
+    <React.Fragment>
+      {paragraph.substring(index, paragraph.length)}
+    </React.Fragment>
+  );
+  return result;
+};
+
+const ReactiveParagraph = ({
+  gender,
+}: {
+  gender: "male" | "female" | "mix";
+}) => {
+  const { paragraph, eventSalientInfo, eventList } = useSelector(
+    (store: RootStoreI) => store.dataReducer
+  );
+  const memoizedContent = useMemo(() => {
+    return generateContent(eventList, eventSalientInfo, paragraph, gender);
+  }, [gender, eventList, eventSalientInfo, paragraph]);
 
   return (
     <React.Fragment>
-      <p className="report--reactive--paragraph">{content}</p>
+      <p className="report--reactive--paragraph">{memoizedContent}</p>
     </React.Fragment>
   );
 };
--- src/Components/Salient/index.tsx
+++ src/Components/Salient/index.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Paragraph from "./ReactiveParagraph";
 import Graph from "./Graph";
 
 const Salient = () => {
+  const [gender, setGender] = useState<"male" | "female" | "mix">("male");
   return (
     <React.Fragment>
-      <Paragraph />
-      <Graph />
+      <button
+        onClick={(e) => {
+          setGender("male");
+        }}
+      >
+        Male
+      </button>
+      <button
+        onClick={(e) => {
+          setGender("female");
+        }}
+      >
+        Female
+      </button>
+      <button
+        onClick={(e) => {
+          setGender("mix");
+        }}
+      >
+        Mix
+      </button>
+      <Paragraph gender={gender} />
     </React.Fragment>
   );
 };
