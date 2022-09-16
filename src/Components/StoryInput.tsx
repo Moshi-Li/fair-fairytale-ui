701ca74fb7d309e63b37b6f8dc9bd9412c6245b4
MoshiLi <li.moshi95@gmail.com>
2022-09-15 23:05:17 -0400

    feat: add new examples (#16)

--- src/Components/StoryInput.tsx
+++ src/Components/StoryInput.tsx
@@ -1,22 +1,41 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-
+import StatusIndicator from "react-status-indicator";
 import { RootStoreI, useAppDispatch } from "../Store";
 import { fetchData } from "../Slices/DataSlice";
 
 import "./StoryInput.scss";
+
+const storyNames = ["ali-baba-and-forty-thieves", "bamboo-cutter-moon-child"];
 
 const StoryInput = () => {
   const { fetching } = useSelector((store: RootStoreI) => store.dataReducer);
   const appDispatchAction = useAppDispatch();
   return (
     <div className="story--input--container">
+      <div className="example--container">
+        <p>Examples:</p>
+        <div className="example--container--list">
+          {storyNames.map((name) => (
+            <button onClick={() => appDispatchAction(fetchData(name))}>
+              {name}
+            </button>
+          ))}
+        </div>
+        <div className="example--container--status">
+          <p>Server Status:</p>
+          <StatusIndicator Negative Pulse />
+        </div>
+      </div>
+
       <textarea className="story--input--textarea"></textarea>
       {!fetching && (
         <button
           className="story--input--btn"
           onClick={() => appDispatchAction(fetchData(""))}
+          disabled
+          title="Server is down"
         >
           Test
         </button>
