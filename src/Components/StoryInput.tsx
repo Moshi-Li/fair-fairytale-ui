18755a715cd65af8f2f003f2eae6c4750425ce3a
Moshi Li <li.moshi95@gmail.com>
2022-12-15 19:11:17 -0500

    Input search bar

--- src/Components/StoryInput.tsx
+++ src/Components/StoryInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import StatusIndicator from "react-status-indicator";
@@ -18,18 +18,44 @@ const storyNames = [
 
 const StoryInput = () => {
   const { fetching } = useSelector((store: RootStoreI) => store.dataReducer);
+  const [searchString, setSearchString] = useState("");
   const appDispatchAction = useAppDispatch();
+
+  const displayingResults = storyNames
+    .filter((name) => {
+      return name.split("-").join(" ").includes(searchString);
+    })
+    .map((name) => name.split("-").join(" "));
   return (
     <div className="story--input--container">
       <div className="example--container">
         <p>Examples:</p>
-        <div className="example--container--list">
-          {storyNames.map((name) => (
-            <button onClick={() => appDispatchAction(fetchData(name))}>
-              {name}
-            </button>
-          ))}
+        <div className="search--bar--container">
+          <input
+            className="search--bar--input"
+            onChange={(e) => setSearchString(e.target.value)}
+          ></input>
+          <p className="search--bar--result">
+            {searchString === ""
+              ? "displaying first 5 results"
+              : `displaying ${displayingResults.length} results for query "${searchString}"`}
+          </p>
         </div>
+
+        <div className="example--container--list">
+          {displayingResults
+            .slice(0, searchString === "" ? 5 : displayingResults.length)
+            .map((name) => (
+              <button
+                onClick={() =>
+                  appDispatchAction(fetchData(name.split(" ").join("-")))
+                }
+              >
+                {name}
+              </button>
+            ))}
+        </div>
+
         <div className="example--container--status">
           <p>Server Status:</p>
           <StatusIndicator Negative Pulse />
