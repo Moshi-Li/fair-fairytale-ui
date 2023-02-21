aff46633280dafaf0f30a11516af4ef45dbdd6f1
Moshi Li <li.moshi95@gmail.com>
2023-02-21 01:29:44 -0500

    feat: connect Plotly graph to Redux Store

--- src/Components/Gender/RatioGraph.tsx
+++ src/Components/Gender/RatioGraph.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Plotly from "plotly.js-dist-min";
 
+import { RootStoreI } from "../../Store";
+
 const RatioGraph = () => {
+  const { dataReducer } = useSelector((store: RootStoreI) => store);
+
+  console.log(dataReducer.characterMeta);
+  console.log(dataReducer.eventMajorList);
+  console.log(dataReducer.eventMeta);
+
   var trace1: Plotly.Data = {
     x: ["2020-10-04", "2021-11-04", "2023-12-04"],
     y: [90, 40, 60],
