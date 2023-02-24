5e94cc2dc54a29394483243e7e51ecc9ef8c87e9
Cássia Sanctos <csamp@ibm.com>
2023-02-24 01:28:00 -0300

    graph doesnt load data

--- src/Components/Gender/RatioGraph.tsx
+++ src/Components/Gender/RatioGraph.tsx
@@ -7,20 +7,37 @@ const RatioGraph = () => {
 
   const { dataReducer } = useSelector((store: RootStoreI) => store);
 
-  console.log(dataReducer.characterMeta);
-  console.log(dataReducer.eventMajorList);
-  console.log(dataReducer.eventMeta);
+  //console.log(dataReducer.characterMeta);
+  //console.log(dataReducer.eventMajorList);
+  //console.log(dataReducer.eventMeta);
+
+   const res: any[] = Object.keys(dataReducer.storyMeta).map((key) => {
+   const { topEvents, counts } = dataReducer.storyMeta;
+
+      return {
+        tevents: topEvents,
+        counts: counts
+      };
+    });
+
+    console.log(dataReducer.storyMeta);
 
   var trace1: Plotly.Data = {
+    x: res.map(item=>{return item.counts.total}),
+    y: res.map(item=>{return item.counts.subject}),
+    type: "bar",
+  };
+
+  var trace2: Plotly.Data = {
     x: [10, 20, 15],
     y: [90, 40, 60],
     type: "bar",
   };
 
-  var data = [trace1];
+  var data = [trace1, trace2];
 
   var layout = {
-    title: "Scroll and Zoom",
+    title: "To be decided",
     showlegend: false,
   };
 
